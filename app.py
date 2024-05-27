ddfba90d99598f4565985164d5a3306a6c7e146a
Diego Jonatan de Miranda <103201121+Djonatan01@users.noreply.github.com>
2024-05-27 12:36:04 -0300

    Ajustes no sistema

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from Src.Router import Router
 app.register_blueprint(Router)
 
 if __name__ == '__main__':
-  app.app_context().push()
-  db.create_all()
+    app.app_context().push()
+    db.create_all()
 
 app.run(debug=True)
